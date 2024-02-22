d4c50abf10064989601a0094e1b591c70336028c
Jhonatan <larico1599@outlook.com>
2024-02-22 16:56:17 -0500

    menus added

--- InventoryApp/Spare.cs
+++ InventoryApp/Spare.cs
@@ -10,7 +10,6 @@
         public string VehicleModel { get; set; }
         public Group Group { get; set; }
 
-        public List<Spare> Spares { get; set; } = new List<Spare>();
 
         public Spare(string? description, string brand, string oemCode, string vehicleBrand, string vehicleModel, Group group)
         {
@@ -20,46 +19,17 @@
             VehicleBrand = vehicleBrand;
             VehicleModel = vehicleModel;
             Group = group;
-
-        }
-        public Spare()
-        {
-            //Sku = GenerateSku();
+            Sku = OemCode + Brand;
         }
 
-        public Spare AddSpare(string? description, string brand, string oemCode, string vehicleBrand, string vehicleModel, Group group)
-        {
-            Spare newSpare = new Spare(description,brand,oemCode,vehicleBrand,vehicleModel,group);
-
-            Spares.Add(newSpare);
-            return newSpare;
-        }
-
-        public List<Spare> GetAllSpare()
-        {
-
-            return Spares;
-        }
-
-        public void GetSpareInfo(string sku)
-        {
-            Spare getSpare =  Spares.Find(s => s.Sku == sku);
-
-            getSpare.ToString();
-        }
 
 
 
         public override string ToString()
         {
-            return $"OEM: {OemCode}\nDescripción: {Description}\n Marca: {Brand}\n Tipo de Vehiculo: {VehicleBrand}, modelo {VehicleModel}\n SKU: {Sku}";
+            return $"OEM: {OemCode}\nDescripción: {Description}\nMarca: {Brand}\nTipo de Vehiculo: {VehicleBrand}, modelo {VehicleModel}\nSKU: {Sku}";
         }
 
-        private string GenerateSku()
-        {
-             
-            return OemCode + Brand; ;
-        }
 
     }
 }
--- InventoryApp/Utilities.cs
+++ InventoryApp/Utilities.cs
@@ -2,23 +2,55 @@
 {
     public class Utilities
     {
-        static Spare newSpare = new Spare();
+
+        private static List<Spare> inventory = new List<Spare>();
         public static void MainMenu()
         {
-            Console.WriteLine("Inventario");
-            Console.WriteLine("Que desea hacer    ?");
-            Console.WriteLine("Agregar un repuesto: 1");
-            Console.WriteLine("Obtener informacion de un repuesto:  2");
+            string option;
+            bool showAllSpares = false;
 
+            do
+            {
+                Console.WriteLine("Inventario");
+                Console.WriteLine("Que desea hacer?");
+                Console.WriteLine("Agregar un repuesto: 1");
+                Console.WriteLine("Obtener informacion de un repuesto:  2");
+                Console.WriteLine("Ver todos los repuestos: 3");
+                Console.WriteLine("Salir: 0");
 
-            AddSpareMenu();
-            GetInfoMenu();
-        }   
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        AddSpareMenu();
+                        break;
+                    case "2":
+                        GetInfoMenu(showAllSpares);
+                        break;
+                    case "3":
+                        showAllSpares = true;
+                        GetInfoMenu(showAllSpares);
+                        showAllSpares = false;
+                        break;
+                    case "0":
+                        Console.WriteLine("Presione enter para salir");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Opción invalida!");
+                        break;
+                }
+                Console.Clear();
+
+            } while (option != "0");
+
+        }
 
         public static void AddSpareMenu()
         {
-            string Code;
-            string SpareBrand;
+            string OemCode;
+            string Brand;
             string Description;
             string VehicleBrand;
             string VehicleModel;
@@ -26,32 +58,58 @@
 
             Console.WriteLine("Ingrese los siguientes datos: ");
             Console.WriteLine("Codigo del repuesto: ");
-            Code = Console.ReadLine();
+            OemCode = Console.ReadLine();
+
             Console.WriteLine("Marca del repuesto: ");
-            SpareBrand = Console.ReadLine();
+            Brand = Console.ReadLine();
+
             Console.WriteLine("Descripción: ");
             Description = Console.ReadLine();
+
             Console.WriteLine("Marca del vehiculo:  ");
             VehicleBrand = Console.ReadLine();
+
             Console.WriteLine("Modelo del vehiculo: ");
             VehicleModel = Console.ReadLine();
+
             Console.WriteLine("En que grupo quiere agruparlo, por ejm caja, corona: ");
-            Enum.TryParse(Console.ReadLine(),true, out Group );
+            Enum.TryParse(Console.ReadLine(), true, out Group);
 
 
-            
-            newSpare.AddSpare(Description,SpareBrand,Code,VehicleBrand,VehicleModel,Group);
+            Spare newSpare = new Spare(Description, Brand,OemCode,VehicleBrand,VehicleModel,Group);
+
+            inventory.Add(newSpare);
 
         }
 
-        public static void GetInfoMenu()
+        public static void GetInfoMenu(bool showAllSpares)
         {
-            string getSku; 
+            if (!showAllSpares)
+            {
+                string getSku;
 
-            Console.WriteLine("Ingrese el SKU del respuesto a buscar: ");
-            getSku = Console.ReadLine();
+                Console.WriteLine("Ingrese el SKU del respuesto a buscar: ");
+                getSku = Console.ReadLine();
 
-            newSpare.GetSpareInfo(getSku);
+                Spare findSpare = inventory.Find(s => s.Sku == getSku);
+
+                string message = findSpare.ToString();
+                Console.WriteLine(message);
+
+                Console.WriteLine("Presione enter para continuar");
+                Console.ReadLine();
+            }
+            else
+            {
+                foreach (var store in inventory)
+                {
+                    Console.WriteLine(store.ToString());
+
+                }
+                Console.WriteLine("Presione enter para continuar");
+                Console.ReadLine();
+            }
+
 
         }
 
